b3a365d8568a276a9d245b70ff9f46cd5a9a5171
Alison WEB <AlisonWEB@yandex.ru>
2024-04-11 01:25:47 +0300

    feat: memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -123,8 +123,13 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  function isCache() {
+    cache = cache || func();
+    return cache;
+  }
+  return isCache;
 }
 
 /**
