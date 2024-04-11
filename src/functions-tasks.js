789b275d86822df501d626faae1113468b29a540
Alison WEB <AlisonWEB@yandex.ru>
2024-04-11 03:00:17 +0300

    feat: getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -223,8 +223,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currId = startFrom;
+  return () => {
+    const logId = currId;
+    currId += 1;
+    return logId;
+  };
 }
 
 module.exports = {
